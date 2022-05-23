585b7caa5fd408b6bddd0b939a0b5bc2dedc7016
Aditya Das <55244625+ThatOneTallKid@users.noreply.github.com>
2022-05-23 13:02:13 +0530

    Time: 26 ms (48.98%), Space: 13.3 MB (30.74%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -1,23 +1,43 @@
 class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
-        int count = 0;
-        set<pair<int,int>> st;
-        int n = nums.size();
-        for(int i =0;i<n;i++) {
-            for(int j =i;j<n;j++) {
-                if(abs(nums[i] - nums[j]) == k and i != j){
-                    if(st.find({nums[i], nums[j]}) == st.end() or st.find({nums[j], nums[i]}) == st.end()) {
-                        count++;
-                        st.insert({nums[i], nums[j]});
-                        st.insert({nums[j], nums[i]});
-                    }
-                    else {
-                        continue;
-                    }
+        // O(n^2)
+        // int count = 0;
+        // set<pair<int,int>> st;
+        // int n = nums.size();
+        // for(int i =0;i<n;i++) {
+        //     for(int j =i;j<n;j++) {
+        //         if(abs(nums[i] - nums[j]) == k and i != j){
+        //             if(st.find({nums[i], nums[j]}) == st.end() or st.find({nums[j], nums[i]}) == st.end()) {
+        //                 count++;
+        //                 st.insert({nums[i], nums[j]});
+        //                 st.insert({nums[j], nums[i]});
+        //             }
+        //             else {
+        //                 continue;
+        //             }
+        //         }
+        //     }
+        // }
+        // return count;
+        
+        // linear solution
+        unordered_map<int,int> mp;
+        for(auto i : nums) {
+            mp[i]++;
+        }
+        int ans =0;
+        for(auto i : mp) {
+            if(k ==0) {
+                if(i.second > 1) {
+                    ans++;
                 }
             }
+            else if(mp.find(i.first+k )!= mp.end()){
+                ans++;
+            }
         }
-        return count;
+        
+        return ans;
     }
 };
